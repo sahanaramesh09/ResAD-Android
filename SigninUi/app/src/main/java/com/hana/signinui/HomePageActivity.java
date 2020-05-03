9f468137191189d7f88e02cfb25310a611c3cb1c
Sahana Ramesh <hanaramesh@Sahanas-MBP.attlocal.net>
2020-05-03 13:58:40 -0700

    Adding Signin signup screens for ResAD Application

---
+++ SigninUi/app/src/main/java/com/hana/signinui/HomePageActivity.java
@@ -0,0 +1,14 @@
+package com.hana.signinui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
